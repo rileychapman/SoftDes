10e556a5775bc26a7d9e537d36fe4c18f2beab15
Riley Chapman <rlychpmn@gmail.com>
2014-01-31 18:32:46 -0500

    Adding grip script

---
+++ hw2/grid.py
@@ -0,0 +1,17 @@
+def plusLine():
+	print '+ ', '- '*4,  '+ ', '- '*4, '+'
+
+def spaceLine():
+	print '| ', ' '*8, '| ', ' '*8, '|'
+
+plusLine()
+spaceLine()
+spaceLine()
+spaceLine()
+spaceLine()
+plusLine()
+spaceLine()
+spaceLine()
+spaceLine()
+spaceLine()
+plusLine()
